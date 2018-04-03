9e99159ebb3a3c764af1839b9d35bf674502a27e
helenmg <elenamg31@gmail.com>
2018-04-03 19:46:40 +0200

    Create endpoint hello world

--- api/asesora.rb
+++ api/asesora.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'sinatra/cross_origin'
 require 'json'
 
+require_relative 'system/actions/factory'
+
 class Asesora < Sinatra::Base
 
   configure do
@@ -11,7 +13,17 @@ class Asesora < Sinatra::Base
   before do
      response.headers['Access-Control-Allow-Origin'] = '*'
   end
+  
+  post '/api/greet' do
+    params = JSON.parse(request.body.read)
+    name = params['name']
+    saludation = Actions::Salutations.greet(name).do
+    saludation.to_json
+  end
 
+  
+  
+  
   options "*" do
     response.headers["Allow"] = "GET, POST, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
--- api/spec/first_spec.rb
+++
@@ -1,7 +0,0 @@
-
-describe 'First spec' do
-  it 'works' do
-
-    expect(true).to eq(true)
-  end
-end
