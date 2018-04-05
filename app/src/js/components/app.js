21e97fbba2ac12699635834033e6d80f28f02718
zerolive <zerolive@gmail.com>
2018-04-05 17:08:36 +0200

    Create Vue component with description

--- app/src/js/components/app.js
+++ app/src/js/components/app.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import TitleAsesora from '../views/title-asesora'
+import DescriptionAsesora from '../views/description-asesora'
 import Version from '../views/version'
 import {Bus} from '../bus'
 
@@ -31,6 +32,7 @@ export default class App {
       data: this.data,
       components: {
         'title-asesora': TitleAsesora,
+        'description-asesora': DescriptionAsesora,
         'version': Version
       }
     })
@@ -39,9 +41,11 @@ export default class App {
   model(){
     return {
       title: "ASESORA",
+      description: 'Registro de asesoramientos técnicos en salud laboral',
       version: "Version 0.0.0",
       setValues:function(values){
         this.title = values.name
+        this.description = values.description
         this.version = values.version
       }
     }
