6bc8b75e2494667e91f121b8aa1e79cad9067d0e
Iris <iriscarballo@gmail.com>
2018-04-06 10:41:48 +0200

    Consolidate rEADMES

---
+++ app/src/js/views/footer-asesora.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <p class="credit">
+    { version }}
+  </p>
+</template>
+
+<script>
+import Version from '../views/version'
+
+export default {
+  name: 'footer-asesora',
+  components: {
+    'version' : Version
+  }
+
+}
+</script>
+
+<style>
+  p {
+    padding: 20px 3.5rem !important;
+  }
+</style>
