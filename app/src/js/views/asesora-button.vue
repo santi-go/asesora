af1a4c2f01ca2766c6a6951ff84dbd85d34691b2
helenmg <elenamg31@gmail.com>
2018-04-18 18:34:45 +0200

    segregate vue views

---
+++ app/src/js/views/asesora-button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button  id="submit"
+            type="button"
+            name="submit"
+            class="submitbutton"
+            v-bind:disabled="true"
+            v-on:click="submit()">
+      {{ labels.submit }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: ['labels', 'values'],
+
+  methods: {
+
+    submit(){
+      let signal = new CustomEvent('submit.solicitude',
+                                  {'detail': {},
+                                  'bubbles': true})
+      this.$el.dispatchEvent(signal)
+      let button = this.$el.querySelector('button')
+      button.value = this.labels.submitting
+      button.disabled = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ app/src/js/views/asesora-text.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <label>{{ labels.text }}</label>
+    <textarea id="solicitude-text"
+              placeholder="*"
+              v-on:blur="onBlur"
+              v-on:focus="onFocus"
+              v-model="values.text">
+    </textarea>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: ['labels', 'values'],
+
+  methods: {
+    onBlur(event){
+      event.target.className = ""
+      if (event.target.value == "") {
+        event.target.className = "error"
+      }
+      this.$parent.enableButton()
+    },
+
+    onFocus(){
+      event.target.className = ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  textarea{
+    min-height: 200px;
+    width: 100%;
+  }
+  textarea::placeholder {
+    text-align: right;
+    font-size: 2em;
+    color: var(--error-color);
+  }
+  .error {
+    border: 1px solid var(--error-color) !important;
+  }
+</style>
