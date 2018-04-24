877d398a22227ca4f9ccc10195b29f12b77df503
Santiago González Erazo <santiago.gonzalez.erazo@gmail.com>
2018-04-24 16:32:09 +0200

    Campo email válido

---
+++ app/src/js/views/solicitude/asesora-email.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <label>{{ labels.email }}:</label>
+    <input  id="email"
+            name="email"
+            placeholder="*"
+            type="text"
+            v-on:keyup="onKeyUp"
+            v-on:focus="onFocus"
+            v-on:keydown="keydown"
+            v-model="values.email"
+            >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'asesora-email',
+
+  props: ['labels', 'values'],
+
+  methods: {
+    onKeyUp(event){
+      event.target.className = ""
+      if (event.target.value == "") {
+        event.target.className = "error"
+      }
+      this.$parent.setButtonStatus()
+    },
+
+    onFocus(){
+      event.target.className = ""
+    },
+
+    keydown(event){
+      let enter = 13
+      if (event.keyCode == enter){
+        event.preventDefault()
+      }
+    },
+
+    emailValidation(){
+      console.log(this.values.email);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  input::placeholder {
+    text-align: right;
+    font-size: 2em;
+    color: var(--error-color);
+    line-height: 1.4em;
+  }
+  input::-webkit-input-placeholder {
+    position: relative;
+    top: 12px;
+  }
+  .error {
+    border: 1px solid var(--error-color) !important;
+  }
+</style>
