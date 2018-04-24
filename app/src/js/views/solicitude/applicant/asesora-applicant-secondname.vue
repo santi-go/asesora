49f882c34f6cb1e4bce8c3620d735af0b36a1a2f
papoi1 <mikelxabier@gmail.com>
2018-04-24 17:14:06 +0200

    Sustituir(ampliar) applicant por tres campos

---
+++ app/src/js/views/solicitude/applicant/asesora-applicant-secondname.vue
@@ -0,0 +1,59 @@
+ <template>
+  <div>
+    <label>{{ labels.applicant.secondname }}:</label>
+    <input  id="secondname"
+            name="secondname"
+            placeholder="*"
+            type="text"
+            v-on:keyup="onKeyUp"
+            v-on:focus="onFocus"
+            v-on:keydown="keydown"
+            v-model="values.applicant.secondname"
+            >
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'asesora-applicant-secondname',
+
+  props: ['labels', 'values'],
+
+  methods: {
+    onKeyUp(event){
+      event.target.className = ""
+      if (event.target.value == "") {
+        event.target.className = "error"
+      }
+      this.$parent.$parent.setButtonStatus()
+    },
+
+    onFocus(){
+      event.target.className = ""
+    },
+
+    keydown(event){
+      if (event.keyCode == 13){
+        event.preventDefault()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  input::placeholder {
+    text-align: right;
+    font-size: 2em;
+    color: var(--error-color);
+    line-height: 1.4em;
+  }
+  input::-webkit-input-placeholder {
+    position: relative;
+    top: 12px;
+  }
+  .error {
+    border: 1px solid var(--error-color) !important;
+  }
+</style>
