f33895830d790cec679545739afdfa75729657ec
papoi1 <mikelxabier@gmail.com>
2018-04-26 17:41:29 +0200

    campo nombre de empresa

--- app/src/js/views/solicitude/company/asesora-company-name.vue
+++ app/src/js/views/solicitude/company/asesora-company-name.vue
@@ -3,7 +3,10 @@
     <label>{{ labels.companyName }}</label>
     <input  id="company-name"
             name="company-name"
+            placeholder="*"
             type="text"
+            v-on:keyup="onKeyUp"
+            v-on:focus="onFocus"
             v-on:keydown="keydown"
             v-model="values.companyName"
             >
@@ -17,6 +20,18 @@ export default {
   props: ['labels', 'values'],
 
   methods: {
+    onKeyUp(event){
+      event.target.className = ""
+      if (event.target.value == "") {
+        event.target.className = "error"
+      }
+      this.$parent.$parent.setButtonStatus()
+    },
+
+    onFocus(){
+      event.target.className = ""
+    },
+
     keydown(event){
       if (event.keyCode == 13){
         event.preventDefault()
@@ -31,4 +46,17 @@ export default {
     margin-top: 1em;
   }
 
+  input::placeholder {
+    text-align: right;
+    font-size: 2em;
+    color: var(--error-color);
+    line-height: 1.4em;
+  }
+  input::-webkit-input-placeholder {
+    position: relative;
+    top: 12px;
+  }
+  .error {
+    border: 1px solid var(--error-color) !important;
+  }
 </style>
