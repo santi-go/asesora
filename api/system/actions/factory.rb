b839e1fe57585a8affc07ecc4abd8c4db11dfd9d
helenmg <elenamg31@gmail.com>
2018-04-27 17:40:24 +0200

    enviar datos de empresa al backend

--- api/system/actions/factory.rb
+++ api/system/actions/factory.rb
@@ -18,7 +18,7 @@ module Actions
     context
   end
 
-  def self.create_solicitude(text, name, surname, email, phonenumber, date)
+  def self.create_solicitude(text, name, surname, email, phonenumber, date, company_name, company_cif, company_employees, company_cnae)
     context = ActionContext.new(Actions::CreateSolicitude)
     context.add(:text, text)
     context.add(:name, name)
@@ -26,6 +26,10 @@ module Actions
     context.add(:email, email)
     context.add(:phonenumber, phonenumber)
     context.add(:date, date)
+    context.add(:company_name, company_name)
+    context.add(:company_cif, company_cif)
+    context.add(:company_employees, company_employees)
+    context.add(:company_cnae, company_cnae)
     context
   end
 
