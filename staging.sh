eb6b74cc14d5ec4f7a2b6fc1a4e9626ce17f71f6
Raül Martínez i Peris <raul@cuchame.es>
2018-05-10 12:32:29 +0200

    Remove mongo container

--- staging.sh
+++ staging.sh
@@ -1,110 +1,184 @@
-#!/bin/bash
+#!/bin/sh
+BLINK='\033[5m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+RESET='\033[0m'
+pause() {
+   printf "\n${BLINK} → Press enter to continue${RESET}"
+   read -p " " key
+   echo "\n"
+}
 
-echo 'Clean staging'
+echo "\n\n\n"
+
+printf "${GREEN}Clean staging\n${RESET}"
+printf " · downing old staging containers: "
 docker-compose -f docker-compose_staging.yml down 2>/dev/null
 DOCKER_STAGING_DOWN=$?
 if [ $DOCKER_STAGING_DOWN -eq 0 ];then
-   echo ' · downing old containers: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · downing old containers: Error ' $DOCKER_STAGING_DOWN
+   printf "${RED}Error ${DOCKER_STAGING_DOWN}${RESET}\n"
+   pause
 fi
 
+printf " · remove folder staging: "
 rm -rf staging 2>/dev/null
 REMOVE_STAGING_FOLDER=$?
 if [ $REMOVE_STAGING_FOLDER -eq 0 ];then
-   echo ' · remove folder staging: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · remove folder staging: Error ' $REMOVE_STAGING_FOLDER
+   printf "\n${RED}Error ${REMOVE_STAGING_FOLDER}${RESET}\n"
+   pause
 fi
 
+printf " · create staging folder: "
 mkdir staging/conf -p 2>/dev/null
 CREATE_STAGING_FOLDER=$?
 if [ $CREATE_STAGING_FOLDER -eq 0 ];then
-   echo ' · create staging folder: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · create staging folder: Error ' $CREATE_STAGING_FOLDER
+   printf "\n${RED}Error ${CREATE_STAGING_FOLDER}${RESET}\n"
+   pause
 fi
 
-echo '\nBuild public architecture:'
+
+printf "${GREEN}\nRunning tests:\n\n${RESET}"
+printf " · launching tests app...\n"
+docker-compose run --rm app npm run test-all 2>/dev/null
+TEST_APP=$?
+if [ $TEST_APP -eq 0 ];then
+   printf " · tests app: ${GREEN}Ok${RESET}\n"
+else
+   printf "\n${RED} · tests app: Error ${TEST_APP}${RESET}\n"
+   pause
+fi
+
+printf " · launching tests api...\n"
+echo "\n"
+docker-compose run --rm api sh -c "gem install bundler && bundle install && bundle exec rake test" 2>/dev/null
+TEST_API=$?
+if [ $TEST_API -eq 0 ];then
+   printf " · tests api: ${GREEN}Ok${RESET}\n"
+else
+   printf "\n${RED} · tests api: Error ${TEST_API}${RESET}\n"
+   pause
+fi
+
+printf " · down docker tests: "
+docker-compose down 2>/dev/null
+DOCKER_TESTING_DOWN=$?
+if [ $DOCKER_TESTING_DOWN -eq 0 ];then
+   printf "${GREEN}Ok${RESET}\n"
+else
+   printf "\n${RED}Error ${DOCKER_TESTING_DOWN}${RESET}\n"
+   pause
+fi
+
+
+printf "${GREEN}\nBuild public architecture:\n\n${RESET}"
+printf " · building public applicative...\n"
 docker-compose run --rm app npm run build 2>/dev/null
+echo "\n"
 BUILD_APP=$?
 if [ $BUILD_APP -eq 0 ];then
-   echo ' · build public applicative: Ok'
+   printf " · build public applicative: ${GREEN}Ok${RESET}\n"
 else
-   echo ' · remove folder staging: Error ' $BUILD_APP
+   printf "\n${RED} · build public applicative: Error ${BUILD_APP}${RESET}\n"
+   pause
 fi
 
+printf " · remove container app: "
 docker-compose down 2>/dev/null
 REMOVE_CONTAINER_APP=$?
 if [ $REMOVE_CONTAINER_APP -eq 0 ];then
-   echo ' · remove container app: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · remove container app: Error ' $REMOVE_CONTAINER_APP
+   printf "\n${RED}Error ${REMOVE_CONTAINER_APP}${RESET}\n"
+   pause
 fi
 
-echo '\nCopy public architecture to staging:'
+printf "${GREEN}\nCopy public architecture to staging:\n${RESET}"
+printf " · copy NGINX conf: "
 cp app/conf/nginx.conf staging/conf/nginx.conf 2>/dev/null
 COPY_NGINX=$?
 if [ $COPY_NGINX -eq 0 ];then
-   echo ' · copy NGINX conf: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy NGINX conf: Error ' $COPY_NGINX
+   printf "\n${RED}Error ${COPY_NGINX}${RESET}\n"
+   pause
 fi
 
+printf " · copy public folder: "
 cp app/public staging/public/ -r 2>/dev/null
 COPY_PUBLIC_FOLDER=$?
 if [ $COPY_PUBLIC_FOLDER -eq 0 ];then
-   echo ' · copy public folder: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy public folder: Error ' $COPY_PUBLIC_FOLDER
+   printf "\n${RED}Error ${COPY_PUBLIC_FOLDER}${RESET}\n"
+   pause
 fi
 
-echo '\nCopy api architecture to staging:'
+printf "${GREEN}\nCopy api architecture to staging:\n${RESET}"
+printf " · copy config.ru: "
 cp api/config.ru staging/config.ru 2>/dev/null
 COPY_CONFIGRU=$?
 if [ $COPY_CONFIGRU -eq 0 ];then
-   echo ' · copy config.ru: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy config.ru: Error ' $COPY_CONFIGRU
+   printf "\n${RED}Error ${COPY_CONFIGRU}${RESET}\n"
+   pause
 fi
 
+printf " · copy Rakefile: "
 cp api/Rakefile staging/Rakefile 2>/dev/null
 COPY_RAKEFILE=$?
 if [ $COPY_RAKEFILE -eq 0 ];then
-   echo ' · copy Rakefile: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy Rakefile: Error ' $COPY_RAKEFILE
+   printf "\n${RED}Error ${COPY_RAKEFILE}${RESET}\n"
+   pause
 fi
 
+printf " · copy asesora.rb: "
 cp api/asesora.rb staging/asesora.rb 2>/dev/null
 COPY_ASESORARB=$?
 if [ $COPY_ASESORARB -eq 0 ];then
-   echo ' · copy asesora.rb: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy asesora.rb: Error ' $COPY_ASESORARB
+   printf "\n${RED}Error ${COPY_ASESORARB}${RESET}\n"
+   pause
 fi
 
+printf " · copy Gemfile: "
 cp api/Gemfile staging/Gemfile 2>/dev/null
 COPY_GEMFILE=$?
 if [ $COPY_GEMFILE -eq 0 ];then
-   echo ' · copy Gemfile: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy Gemfile: Error ' $COPY_GEMFILE
+   printf "\n${RED}Error ${COPY_GEMFILE}${RESET}\n"
+   pause
 fi
 
+printf " · copy system: "
 cp api/system staging/system/ -r 2>/dev/null
 COPY_SYSTEM=$?
 if [ $COPY_SYSTEM -eq 0 ];then
-   echo ' · copy system: Ok'
+   printf "${GREEN}Ok${RESET}\n"
 else
-   echo ' · copy system: Error ' $COPY_SYSTEM
+   printf "\n${RED}Error ${COPY_SYSTEM}${RESET}\n"
+   pause
 fi
 
-echo '\nLaunching staging:'
-docker-compose -f docker-compose_staging.yml up --build 2>/dev/null
+printf "${GREEN}\nLaunching staging in local environment:\n${RESET}"
+echo "\n"
+docker-compose -f docker-compose_staging.yml up --build -d 2>/dev/null
 UPPING_STAGING=$?
 if [ $UPPING_STAGING -eq 0 ];then
-   echo ' · upping staging: Ok'
+   printf " · upping staging: ${GREEN}Ok${RESET}\n"
 else
-   echo ' · upping staging: Error ' $UPPING_STAGING
+   printf "\n${RED} · upping staging: Error ${UPPING_STAGING}${RESET}\n"
+   pause
 fi
+
+echo "${GREEN}\n\n${BLINKING}Enter in localhost to view the staging running!${RESET}"
