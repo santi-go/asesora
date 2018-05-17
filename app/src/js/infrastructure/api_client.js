ce271f00ffc0facd0de4af172c3174dd5d4e784f
papoi1 <mikelxabier@gmail.com>
2018-05-17 17:04:10 +0200

    [APP] Interpolar API_PORT en api_client

--- app/src/js/infrastructure/api_client.js
+++ app/src/js/infrastructure/api_client.js
@@ -1,13 +1,15 @@
 import aja from 'aja'
 
 export let APIClient = {
-  BASE_URL : 'http://' + process.env.API_HOST + ':' + process.env.API_PORT.toString() + '/api/',
+  HOST : process.env.API_HOST,
+  PORT : process.env.API_PORT,
 
   hit: function (endpoint, data, action){
+    let base_url = `http://${this.HOST}:${this.PORT}/api/`
     aja()
     .method('post')
     .body(data)
-    .url(this.BASE_URL + endpoint)
+    .url(base_url + endpoint)
     .on('success', action)
     .go();
   }
