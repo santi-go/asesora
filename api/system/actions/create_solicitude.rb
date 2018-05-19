958a317c83b455e9101691ac0403e9e706ae323a
helenmg <elenamg31@gmail.com>
2018-05-19 11:08:19 +0200

    refactor action create solicitude

--- api/system/actions/create_solicitude.rb
+++ api/system/actions/create_solicitude.rb
@@ -4,9 +4,39 @@ require_relative '../services/companies/service'
 module Actions
   class CreateSolicitude
     def self.do(text:, name:, surname:, email:, phonenumber:, date:, company_name:, company_cif:, company_employees:, company_cnae:)
-      company = ::Companies::Service.create(company_name, company_cif, company_employees, company_cnae)
-      result = ::Solicitudes::Service.create(text, name, surname, email, phonenumber, date, company.identify())
-      result
+      company = create_company(company_name, company_cif, company_employees, company_cnae)
+      solicitude = create_solicitude(text, name, surname, email, phonenumber, date, company)
+
+      solicitude
+    end
+
+    private
+
+    def self.create_company(name, cif, employees, cnae)
+      ::Companies::Service.create(
+        name,
+        cif,
+        employees,
+        cnae
+      )
+    end
+
+    def self.create_solicitude(text, name, surname, email, phonenumber, date, company)
+      company_id = identify(company)
+
+      ::Solicitudes::Service.create(
+        text,
+        name,
+        surname,
+        email,
+        phonenumber,
+        date,
+        company_id
+      )
+    end
+
+    def self.identify(company)
+      company.identify()
     end
   end
 end
