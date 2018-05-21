6f3c2dd19922fd098371c95f7311a48f68783ad7
Raül Martínez i Peris <raul@cuchame.es>
2018-05-21 11:08:57 +0200

    refactor test para que use validationCif como método estático

--- app/test/unit/validation_cif_test.js
+++ app/test/unit/validation_cif_test.js
@@ -1,12 +1,13 @@
 const expect = require('chai').expect
 
-import ValidationCif from '../../src/js/library/validation-cif'
+import {ValidationCif} from '../../src/js/library/validation-cif'
+
 
 describe('Validation CIF', () => {
   it('valides a correct CIF/NIF', ()=> {
     let list = ["A01316637", "B91735456", "B41745100", "B63765754", "B63964506", "B91874164", "G23536238", "G31947351", "B97539795", "B74267485", "F93020618", "23801250E", "39727019q", "Y2612493Y", "X4945396M", "06033994J", "00820673X"]
     let result = ""
-    let validationCif = new ValidationCif()
+    let validationCif = ValidationCif
 
     list.forEach(function(value) {
       result = validationCif.validate(value)
@@ -17,7 +18,7 @@ describe('Validation CIF', () => {
   it('valides a incorrect CIF/NIF', ()=> {
     let list = ["A0131X637", "B9735456", "41745100", "z63765754", "Y63964506", "B61874164", "G2338", "23800250E", "39727019X", "Z2612493Y"]
     let result = ""
-    let validationCif = new ValidationCif()
+    let validationCif = ValidationCif
 
     list.forEach(function(value) {
       result = validationCif.validate(value)
