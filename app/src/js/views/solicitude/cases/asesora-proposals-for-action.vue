1a5e06b2334502b3397095213e6cac2473d00295
helenmg <elenamg31@gmail.com>
2018-06-14 18:05:55 +0200

    Campo Propuestas de actuación de texto libre sin formato

---
+++ app/src/js/views/solicitude/cases/asesora-proposals-for-action.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <label>{{ labels.proposals }}</label>
+    <textarea id="cases-proposals"
+              v-model="values.proposals">
+    </textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'asesora-proposals-for-action',
+
+  props: ['labels', 'values']
+}
+</script>
