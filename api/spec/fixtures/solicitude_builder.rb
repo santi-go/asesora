0bf2c892c6e0c578614897165ed69ce0780eb50c
Vicente Baixauli <vibaiher@gmail.com>
2018-06-21 23:59:18 +0200

    fixup! [MOB] Creación de solicitudes

---
+++ api/spec/fixtures/solicitude_builder.rb
@@ -0,0 +1,47 @@
+class SolicitudeBuilder
+  def self.default
+    new()
+  end
+
+  def initialize
+    @solicitude = {
+      applicantId: "",
+      applicantName: Fixtures::APPLICANT_NAME,
+      applicantSurname: Fixtures::APPLICANT_SURNAME,
+      applicantEmail: Fixtures::APPLICANT_EMAIL,
+      applicantPhonenumber: Fixtures::APPLICANT_PHONENUMBER,
+      text: Fixtures::TEXT,
+      date: Fixtures::DATE,
+      companyName: Fixtures::COMPANY_NAME,
+      companyCif: Fixtures::COMPANY_CIF
+    }
+  end
+
+  def with
+    self
+  end
+
+  def text(value)
+    @solicitude[:text] = value
+    self
+  end
+
+  def company_name(value)
+    @solicitude[:companyName] = value
+    self
+  end
+
+  def date(value)
+    @solicitude[:date] = value
+    self
+  end
+
+  def creation_moment(value)
+    @solicitude[:creation_moment] = value
+    self
+  end
+
+  def build
+    @solicitude
+  end
+end
