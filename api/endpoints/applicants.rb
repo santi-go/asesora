f3c8266990c6790b9de9179fc10ef25497b4888c
helenmg <elenamg31@gmail.com>
2018-06-22 18:23:48 +0200

    Reestructurar endpoints API

---
+++ api/endpoints/applicants.rb
@@ -0,0 +1,40 @@
+require_relative '../system/actions/update_applicant'
+require_relative '../system/actions/retrieve_solicitudes'
+
+module Endpoints
+  class Applicants
+    def self.define_update_applicant(api)
+      api.post '/api/update-applicant' do
+        params = JSON.parse(request.body.read)
+        data = {
+          name:params['applicantName'],
+          surname:params['applicantSurname'],
+          email:params['applicantEmail'],
+          phonenumber:params['applicantPhonenumber'],
+          id:params['applicantId']
+        }
+    
+        updated = Actions::UpdateApplicant.do(data)
+        return {}.to_json if updated.nil?
+    
+        updated.to_json
+      end
+    end
+
+    def self.define_applicant_matches(api)
+      api.post '/api/applicant-matches' do
+        params = JSON.parse(request.body.read)
+    
+        criteria = {
+          name: params['applicantName'],
+          surname: params['applicantSurname'],
+          phonenumber: params['applicantPhonenumber'],
+          email: params['applicantEmail']
+        }
+    
+        applicants = Actions::RetrieveSolicitudes.do_applicants(criteria)
+        {data: applicants}.to_json
+      end
+    end
+  end
+end
