58b7ffdd3c3dcea3a2e3a456a3b65c7f0774c356
Santiago González Erazo <santiago.gonzalez.erazo@gmail.com>
2018-06-26 19:05:46 +0200

    Validación para usar el botón "crear asesoramiento" - test

--- app/test/e2e/solicitude_subject_test.js
+++ app/test/e2e/solicitude_subject_test.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect
 const assert = require('chai').assert
 const SolicitudesList = require('./page-object/solicitudes-list')
 const SolicitudeSubjects = require('./page-object/solicitude-subjects')
+const Solicitude = require('./page-object/solicitude')
 const Fixtures = require('./fixtures')
 
 describe('Solicitude Subjects', () => {
@@ -17,12 +18,14 @@ describe('Solicitude Subjects', () => {
 
   it("are rendered in a subject list within edition solicitude", function () {
     const newProposal = "A new proposal"
+    const newAnalysis = "Other Analysis"
     const solicitudesList = new SolicitudesList()
     solicitudesList.waitForSolicitudesList()
     solicitudesList.clickOnShowButtonOnItem(1)
 
     const solicitudeSubjects = new SolicitudeSubjects()
     solicitudeSubjects.clickOnAddSubjectButton()
+    solicitudeSubjects.fill().subjectAnalysis(newAnalysis).lostFocus()
     solicitudeSubjects.fill().subjectProposal(newProposal).lostFocus()
     solicitudeSubjects.waitForCreateCounseling()
     solicitudeSubjects.clickOnCreateCounseling()
@@ -32,4 +35,25 @@ describe('Solicitude Subjects', () => {
     expect(result).to.eq(true)
   })
 
+  it("enables button when there is an analysis", function () {
+
+    const newAnalysis = "Other Analysis"
+    const solicitude = new Solicitude()
+
+    solicitude.required().submit()
+    
+    const solicitudesList = new SolicitudesList()
+
+    solicitudesList.waitForSolicitudesList()
+    browser.saveScreenshot("test.png")
+    solicitudesList.clickOnShowButtonOnItem(1)
+
+    const solicitudeSubjects = new SolicitudeSubjects()
+    solicitudeSubjects.clickOnAddSubjectButton()
+    solicitudeSubjects.fill().subjectAnalysis(newAnalysis).lostFocus()
+
+    result = solicitudeSubjects.isCreateCounselingEnabled()
+
+    expect(result).to.eq(true)
+  })
 })
