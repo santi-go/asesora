ff4df16a852766a7915bacbb71028a4a926936cc
papoi1 <mikelxabier@gmail.com>
2018-06-28 16:03:20 +0200

    mejora template subjects

--- app/src/js/views/solicitude/cases/asesora-subjects-list.vue
+++ app/src/js/views/solicitude/cases/asesora-subjects-list.vue
@@ -3,36 +3,39 @@
     <div class="card-title">
       <h3>{{ labels.subjectsList }}</h3>
     </div>
-    <table>
-      <tr v-for="item in values.subjects">
-        <td>
-          <div class="row grid-responsive">
-            <div class="column column-33">
-              <h4>Caso {{values.subjects.indexOf(item) +1 }}:</h4>
-            </div>
-            <div class="column column-67">
-              <label>{{ labels.proposals }}</label>
-              <template v-if=" item.proposal =='' "><p>{{ labels.notApply }}</p></template>
-              <template v-else><p>{{ item.proposal }}</p></template>
 
-              <label>{{ labels.analysis }}</label>
-              <template v-if=" item.analysis =='' "><p>{{ labels.notApply }}</p></template>
-              <template v-else><p>{{ item.analysis }}</p></template>
+    <div>
+      <table>
+        <tr v-for="item in values.subjects">
+          <td>
+            <div class="row grid-responsive">
+              <div class="column column-20">
+                <h4>Caso {{values.subjects.indexOf(item) +1 }}:</h4>
+              </div>
+              <div class="column column-80">
+                <label>{{ labels.proposals }}</label>
+                <template v-if=" item.proposal =='' "><p>{{ labels.notApply }}</p></template>
+                <template v-else><p>{{ item.proposal }}</p></template>
 
-              <label>{{ labels.topics }}</label>
-              <template v-if=" item.topics.length == 0 "><p>{{ labels.notApply }}</p></template>
-              <template v-else>
-                <tr v-for="topic in item.topics">
-                  <td>
-                    {{ topic }}
-                  </td>
-                </tr>
-              </template>
+                <label>{{ labels.analysis }}</label>
+                <template v-if=" item.analysis =='' "><p>{{ labels.notApply }}</p></template>
+                <template v-else><p>{{ item.analysis }}</p></template>
+
+                <label>{{ labels.topics }}</label>
+                <template v-if=" item.topics.length == 0 "><p>{{ labels.notApply }}</p></template>
+                <template v-else>
+                  <tr v-for="topic in item.topics">
+                    <td>
+                      {{ topic }}
+                    </td>
+                  </tr>
+                </template>
+              </div>
             </div>
-          </div>
-        </td>
-      </tr>
-    </table>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
